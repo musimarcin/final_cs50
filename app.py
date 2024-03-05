5f20752b049d905710e23cc9d3974d8646e5edb2
musimarcin <marcinm994@gmail.com>
2024-03-05 21:12:02 +0100

    baseground

--- app.py
+++ app.py
@@ -7,3 +7,20 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+def start():
+    """ITS BASE"""
+    session.clear()
+    return render_template("index.html")
