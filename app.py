2ff762d528347d7e3e9f3a9dc5dbda0a70191803
musimarcin <marcinm994@gmail.com>
2024-03-09 09:29:08 +0100

    updated login and register windows

--- app.py
+++ app.py
@@ -21,24 +21,65 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-@app.route("/")
-def start():
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
     session.clear()
-    datew = calendar.day_name
-    datey = datetime.now().year
+
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        session["user_id"] = rows[0]["id"]
+
+        return redirect("/")
+
+    else:
+        return render_template("login.html")
+    
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    session.clear()
+    name = request.form.get("username")
+    pwd = request.form.get("password")
+    pwd2 = request.form.get("confirmation")
+    if request.method == "POST":
+        if not name:
+            return apology("must provide username", 400)
+        elif not pwd:
+            return apology("must provide password", 400)
+        elif not pwd2:
+            return apology("must provide password2", 400)
+        elif str(pwd) != str(pwd2):
+            return apology("passwords does not match", 400)
+        else:
+            rows = db.execute("SELECT * FROM users WHERE username = ?", name)
+            if len(rows) > 0:
+                return apology("user already exists", 400)
+            else:
+                db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", name, generate_password_hash(pwd, method='pbkdf2', salt_length=16))
+    return render_template("register.html")
+
+@app.route("/", methods=["GET", "POST"])
+def start():
+
     datem = datetime.now().month
+    datew = calendar.day_abbr
+    datey = datetime.now().year
     month = calendar.month_name[datem]
     cal = calendar.monthcalendar(datey, datem)
 
-    return render_template("index.html", cal=cal, month=month, datew=datew)
-
-
-@app.route("/change", methods=["GET", "POST"])
-def changep():
     if request.method == "POST":
-        datem = datetime.now().month
-        datew = calendar.day_name
-        datey = datetime.now().year
         current_month = request.form.get("current_month").capitalize()
         for i in range(len(calendar.month_name)):
             if calendar.month_name[i] == current_month:
@@ -52,4 +93,9 @@ def changep():
         month = calendar.month_name[datem]
         cal = calendar.monthcalendar(datey, datem)
         return render_template("index.html", cal=cal, month=month, datew=datew)
+    else:
+        return render_template("index.html", cal=cal, month=month, datew=datew)
     
+
+if __name__ == "__main__":
+    app.run(debug=True)
